83e354fc90e35ba73a106de2bc971f2c1b662243
paruyrsahakyan <paruyrsahakyan@gmail.com>
2019-01-07 02:03:29 +0400

    fixing some bugs in recently created entities

--- src/main/java/group/service/iko/entities/PeriodicMaintenance.java
+++ src/main/java/group/service/iko/entities/PeriodicMaintenance.java
@@ -12,7 +12,7 @@ public class PeriodicMaintenance {
     private int id;
     @Column
     private int smr;
-    @OneToMany(mappedBy = "periodic_maintenence", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "periodic_maintenance", fetch = FetchType.EAGER)
     @Cascade(org.hibernate.annotations.CascadeType.ALL)
     private List<MaintenancePart> maintenanceParts;
     @Column
