0608846c81a35559f7682da7bf5cf3586f1a5d4d
paruyrsahakyan <paruyrsahakyan@gmail.com>
2019-02-15 00:47:27 +0400

    added controller method for downloading ServiceReport

--- src/main/java/group/service/iko/service/StorageService.java
+++ src/main/java/group/service/iko/service/StorageService.java
@@ -91,5 +91,28 @@ public class StorageService {
         }
     }
 
+    public String getFilePath() {
+        return filePath;
     }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getWareHouseFileFolder() {
+        return wareHouseFileFolder;
+    }
+
+    public String getWareHouseFileName() {
+        return wareHouseFileName;
+    }
+}
 
