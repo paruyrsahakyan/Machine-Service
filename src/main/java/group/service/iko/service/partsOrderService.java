19518e02e2fa0d8c8b3f6cf24918d14883b4d565
paruyrsahakyan <paruyrsahakyan@gmail.com>
2019-03-03 12:30:03 +0400

    adding the feature of viewing machineList that are maintained by IKO

--- src/main/java/group/service/iko/service/partsOrderService.java
+++ src/main/java/group/service/iko/service/partsOrderService.java
@@ -2,13 +2,15 @@ package group.service.iko.service;
 
 import group.service.iko.entities.Part;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class partsOrderService {
 
 
     public List<Part> countParts(int days) {
-
+List<Part> partList = new ArrayList<>();
+return partList;
     }
 
 
