d26fe79869b71abad2e09adc00eede29d5c57b1b
IKO <paruyrsahakyan@gmail.com>
2020-04-24 23:42:13 +0400

    changed pasword for ROLE_ADMIN

--- src/main/java/group/service/iko/Test.java
+++ src/main/java/group/service/iko/Test.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class Test {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder  = new BCryptPasswordEncoder();
-        String encoded = encoder.encode("ikouser");
+        String encoded = encoder.encode("meadmin");
         System.out.println(encoded);
 
     }
